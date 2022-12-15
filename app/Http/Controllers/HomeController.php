9deffd49a1d5c29d9e7d9e71aa82d03ee1e0c6c8
aamir <maamir.se.uol@gmail.com>
2022-12-15 16:20:07 +0500

    one role added in the table

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 class HomeController extends Controller
 {
     /**
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        Role::create(['name'=>'writer']);
         return view('home');
     }
 }
